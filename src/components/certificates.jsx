818f971a7aef635dd90110313a9106c797f40262
rahul181203 <rahulkanakam123@gmail.com>
2021-11-29 15:26:15 +0530

    added 4

--- src/components/certificates.jsx
+++ src/components/certificates.jsx
@@ -13,6 +13,7 @@ function Certificates(){
             <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
         </ol>
         <div className="carousel-inner">
             <div className="carousel-item active">
